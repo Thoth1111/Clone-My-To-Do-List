31a0efee994e203e12696e53a89636c8117e7327
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-01-24 13:23:04 +0300

    Made changes based on review

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,15 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+               limit: 8192,               
+             },
+          },
+      ],
       },
-    ],
+    ],    
   },
 };
